1d06646b59ccec7136b5e239708e504334623c01
Anikett700 <kushwahaanitak@gmail.com>
2026-04-21 01:59:50 +0530

    Add initial HTML and JavaScript files for Cravings project and basic JavaScript examples
    - Created index.html for Cravings project with Bootstrap integration, navigation, and a carousel section.
    - Added a section for featured restaurants with cards.
    - Introduced index.html and script.js for basic JavaScript exercises, demonstrating variable declarations and console logging.

---
+++ JavaScript/Day1-Basic/script.js
@@ -0,0 +1,34 @@
+console.log("Hello World!")
+console.log("I Am Anikett!!!")
+
+let a = 5;
+// let a = 3;      can't repeat same variable name by using let
+
+a = 10;
+
+var x = 10;               // value can change 
+var x = 20;               // update 
+var y = "30"
+
+const pi = 3.14;        // value cannot change
+// pi = 41;
+
+
+
+
+console.log("task complete")
+
+console.log(x=x);
+console.log(x==x);
+console.log(x===x);
+
+// console.log(x=y);
+console.log(x==y);
+console.log(x===y);
+
+
+console.log(typeof x);
+console.log(typeof y);
+
+
+
