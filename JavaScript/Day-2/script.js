a424f26feb90ce77f7cbbb6c84844e62935997c8
Anikett700 <kushwahaanitak@gmail.com>
2026-04-29 19:32:05 +0530

    Add initial HTML and JavaScript files with loop examples

---
+++ JavaScript/Day-2/script.js
@@ -0,0 +1,51 @@
+
+// ------------------ FOR LOOP ---------------- //
+for(var i = 1; i <= 10; i++){
+    
+    console.log(2*i);
+    // console.log(i, "=" ,i*i);   
+}
+
+// -------------------- WHILE LOOP -----------------//
+let b = 1;
+while(b<=10){
+    console.log(2*b);
+    b++;
+}
+
+// --------------------- DO WHILE LOOP ------------- //
+let c = 1;
+do{
+    console.log(2*c);
+    c++;
+}while(c<=10);
+
+
+// ------------------------------------- Array --------------------------------- //
+
+
+// let a = [24, 58, 33, 86, 96, 12, 53, 23, 32, 100]
+
+// // ------------------ FOR LOOP ---------------- //
+
+// for(var k = 0; k < 10; k++){
+//     console.log(a[k]);
+// }
+
+// // -------------------- WHILE LOOP -----------------//
+
+// let i = 0;                            // initialization
+// while(i<10){                          // condition
+//     console.log(a[i]);                // working
+//     i++;                              // updation
+// }
+
+
+// // --------------------- DO WHILE LOOP ------------- //
+
+// let x = 0;
+// do{
+//     console.log(a[x]);
+//     x++;
+// }while(x < 10);
+
