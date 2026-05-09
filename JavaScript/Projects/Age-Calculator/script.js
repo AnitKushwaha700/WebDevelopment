e471eae34c1b90b1ac4a20912e35a98a0184e611
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-09 20:04:27 +0530

    Add Age Calculator project with HTML structure and JavaScript functionality for age calculation

---
+++ JavaScript/Projects/Age-Calculator/script.js
@@ -0,0 +1,18 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const dob = document.getElementById("DOB").value;
+  const currDate = document.getElementById("CurrentDate").value;
+
+  let curYear = currDate.split("-");
+  console.log(curYear);
+
+  const Age = Number(currDate.split("-")[0]) - Number(dob.split("-")[0]);
+  const month = Number(currDate.split("-")[1]) - Number(dob.split("-")[1]);
+  const day = Number(currDate.split("-")[2]) - Number(dob.split("-")[2]);
+
+  document.getElementById("DOB").value = "";
+  document.getElementById("CurrentDate").value = "";
+
+  document.getElementById("MyAge").innerText = Age + "-" + month + "-" + day;
+});
