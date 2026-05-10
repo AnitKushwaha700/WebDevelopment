936e8f4d5e348d73b5b776dd24abdf7cda54043b
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-10 20:02:27 +0530

    Add Tip Calculator project with HTML structure, CSS styling, and JavaScript functionality

---
+++ JavaScript/Day-11CALCULATOR/script.js
@@ -0,0 +1,9 @@
+// document.querySelector("button").addEventListener("click" , (event) => {
+// console.log(event.target);
+
+// });
+
+
+function Calculate(data) {
+    
+}
--- JavaScript/Projects/Tip-Calculator/script.js
+++ JavaScript/Projects/Tip-Calculator/script.js
@@ -1,18 +1,27 @@
-document.querySelector("form").addEventListener("submit", (event) => {
+// document.querySelector("form").addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const dob = document.getElementById("DOB").value;
+//   const currDate = document.getElementById("CurrentDate").value;
+
+//   let curYear = currDate.split("-");
+//   console.log(curYear);
+
+//   const Age = Number(currDate.split("-")[0]) - Number(dob.split("-")[0]);
+//   const month = Number(currDate.split("-")[1]) - Number(dob.split("-")[1]);
+//   const day = Number(currDate.split("-")[2]) - Number(dob.split("-")[2]);
+
+//   document.getElementById("DOB").value = "";
+//   document.getElementById("CurrentDate").value = "";
+
+//   document.getElementById("MyAge").innerText = Age + "-" + month + "-" + day;
+// });
+
+
+document.querySelector("form").addEventListener("submit" , (event) =>{
   event.preventDefault();
 
-  const dob = document.getElementById("DOB").value;
-  const currDate = document.getElementById("CurrentDate").value;
+  const amount = document.getElementById()
 
-  let curYear = currDate.split("-");
-  console.log(curYear);
 
-  const Age = Number(currDate.split("-")[0]) - Number(dob.split("-")[0]);
-  const month = Number(currDate.split("-")[1]) - Number(dob.split("-")[1]);
-  const day = Number(currDate.split("-")[2]) - Number(dob.split("-")[2]);
-
-  document.getElementById("DOB").value = "";
-  document.getElementById("CurrentDate").value = "";
-
-  document.getElementById("MyAge").innerText = Age + "-" + month + "-" + day;
-});
+})
