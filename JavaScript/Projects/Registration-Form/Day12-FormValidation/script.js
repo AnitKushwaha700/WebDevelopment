678dbeb96a7a46e572c153d53180080ff210bb46
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-13 20:49:33 +0530

    Refactor: update registration form layout and remove validation logic from script

--- JavaScript/Projects/Registration-Form/Day12-FormValidation/script.js
+++ JavaScript/Projects/Registration-Form/Day12-FormValidation/script.js
@@ -6,29 +6,4 @@ function Submit() {
 
   const ps = document.getElementById("password").value;
 
-
-
-
-  // Name Validation
-  if (!/^[A-Za-z ]+$/.test(fn)) {
-    document.getElementById("nameError").innerText =
-      "Invalid Full Name";
-    return;
-  }
-
-  // Email Validation
-  if (!/^[A-Za-z._\d]+@gmail\.com$/.test(em)) {
-    document.getElementById("emailError").innerText =
-      "Invalid Email";
-    return;
-  }
-
-  // Phone Validation
-  if (!/^[6-9]\d{9}$/.test(ph)) {
-    document.getElementById("phoneError").innerText =
-      "Invalid Phone Number";
-    return;
-  }
-
-  alert("Form Submitted Successfully");
 }
