914748804d4c436094e027da2f8776872d5986e4
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-14 20:17:25 +0530

    Refactor: enhance registration form structure and improve submission logic with detailed data handling

--- JavaScript/Projects/Registration-Form/Day12-FormValidation/script.js
+++ JavaScript/Projects/Registration-Form/Day12-FormValidation/script.js
@@ -1,22 +1,68 @@
-function submitForm() {
-  const name = document.getElementById("fullName").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
+document
+  .getElementById("registrationForm")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  if (name === "") {
-    alert("Enter your name");
-    return;
-  }
+    // personal details
+    const fn = document.getElementById("fullName").value;
+    const em = document.getElementById("email").value;
+    const pn = document.getElementById("number").value;
+    const gndr = document.querySelector("input[name='gender']:checked")?.value;
+    const dob = document.getElementById("DOB").value;
 
-  if (email === "") {
-    alert("Enter your email");
-    return;
-  }
+    // Academic details
+    const qual = document.getElementById("Qualification").value;
+    const grd = document.getElementById("Grade").value;
 
-  if (phone === "") {
-    alert("Enter your phone number");
-    return;
-  }
+    // Course information
+    const crs = document.getElementById("Course").value;
 
-  alert("Form Submitted Successfully");
-}
+    const timings = [];
+    document
+      .querySelectorAll("input[name='timings']:checked")
+      .forEach((item) => {
+        timings.push(item.value);
+      });
+
+    // Address
+    const adrs = document.getElementById("Address").value;
+    const cty = document.getElementById("City").value;
+    const pinc = document.getElementById("pinCode").value;
+
+    // Guardians Details
+    const guarName = document.getElementById("guardianName").value;
+    const guarNum = document.getElementById("guardianNumber").value;
+
+    // Additional information 
+    const ref = document.getElementById("reference").value;
+    const spclReq = document.getElementById("specialRequirements").value;
+
+    console.log({
+      fn,
+      em,
+      pn,
+      gndr,
+      dob,
+      qual,
+      grd,
+      crs,
+      timings,
+      adrs,
+      cty,
+      pinc,
+      guarName,
+      guarNum,
+      ref,
+      spclReq,
+    });
+
+    
+  });
+
+document
+  .getElementById("registrationForm")
+  .addEventListener("reset", (event) => {
+    event.preventDefault();
+
+    window.location.reload();
+  });
