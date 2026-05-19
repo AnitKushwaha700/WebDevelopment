8b9110743c6da012219dc7631f4cd05b5e3b56ca
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-19 20:01:35 +0530

    Refactor: remove unused HTML and JavaScript files for Callback and Storage projects; add new files for Promise implementation

---
+++ JavaScript/Dat12-Promise/script.js
@@ -0,0 +1,23 @@
+function getData(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value === 3) {
+        reject("Query Not Found");
+      } else {
+        console.log("Data Sent", value);
+        resolve("Query Solved");
+      }
+    }, 1000);
+  });
+}
+
+// const myPromise = getData(1);
+
+getData(1)
+  .then(() => getData(2))
+  .then(() => getData(3))
+  .then(() => getData(4))
+  .then(() => getData(5))
+  .then(() => getData(6))
+  .catch((rej) => console.log(rej)
+  );
