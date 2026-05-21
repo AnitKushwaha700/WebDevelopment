ff1cbdf96480fd0bccfedb2d61fa6420cfdc07aa
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-21 20:28:24 +0530

    Refactor: enhance Travel Bill Calculator UI and implement bill calculation logic

--- JavaScript/Projects/Travel-Bill-Calculator/script.js
+++ JavaScript/Projects/Travel-Bill-Calculator/script.js
@@ -1,18 +1,51 @@
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault();
+// Button select
+let calculateBtn = document.getElementById("calculateBtn");
 
-  const dob = document.getElementById("DOB").value;
-  const currDate = document.getElementById("CurrentDate").value;
+// Button click event
+calculateBtn.addEventListener("click", function () {
+  // Input value
+  let km = document.getElementById("kmInput").value;
 
-  let curYear = currDate.split("-");
-  console.log(curYear);
+  // Elements select
+  let error = document.getElementById("error");
+  let resultSection = document.getElementById("resultSection");
+  let totalBill = document.getElementById("totalBill");
+  let breakdown = document.getElementById("breakdown");
 
-  const Age = Number(currDate.split("-")[0]) - Number(dob.split("-")[0]);
-  const month = Number(currDate.split("-")[1]) - Number(dob.split("-")[1]);
-  const day = Number(currDate.split("-")[2]) - Number(dob.split("-")[2]);
+  // Validation
+  if (km === "" || km <= 0) {
+    error.innerHTML = "Please enter valid kilometers";
+    resultSection.classList.add("d-none");
+    return;
+  }
 
-  document.getElementById("DOB").value = "";
-  document.getElementById("CurrentDate").value = "";
+  // Remove error
+  error.innerHTML = "";
 
-  document.getElementById("MyAge").innerText = Age + "-" + month + "-" + day;
+  // Convert string into number
+  km = Number(km);
+
+  // Bill calculation
+  let bill = 0;
+
+  if (km <= 10) {
+    bill = km * 11;
+  } else if (km <= 30) {
+    bill = 10 * 11 + (km - 10) * 10;
+  } else {
+    bill = 10 * 11 + 20 * 10 + (km - 30) * 8;
+  }
+
+  // Show result
+  totalBill.innerHTML = "₹ " + bill;
+
+  // Breakdown show
+  breakdown.innerHTML = `
+    First 10 KM = ₹11 per KM <br>
+    Next 20 KM = ₹10 per KM <br>
+    Above 30 KM = ₹8 per KM
+  `;
+
+  // Show result section
+  resultSection.classList.remove("d-none");
 });
