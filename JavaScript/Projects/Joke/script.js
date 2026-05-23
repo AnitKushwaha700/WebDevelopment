ebafb36d915ca7a2c4134dabe3f95cf8a00df7f9
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-23 20:55:41 +0530

    Add joke fetching functionality and update HTML structure

---
+++ JavaScript/Projects/Joke/script.js
@@ -0,0 +1,23 @@
+async function GetNewJoke() {
+  const API_URL = "https://official-joke-api.appspot.com/jokes/random";
+
+  const response = await fetch(API_URL);
+
+  const data = await response.json();
+
+  //   if (data.tpye === "single") {
+  //     document.getElementById("setup").innerText = data.joke;
+  //     document.getElementById("delivery").innerText = "";
+  //   } else {
+  //     document.getElementById("setup").innerText = data.setup;
+  //     document.getElementById("delivery").innerText = data.delivery;
+  //   }
+
+  // document.getElementById("setup").innerText = data.joke || data.setup;
+  // document.getElementById("delivery").innerText = data.delivery || "";
+
+
+  
+  document.getElementById("setup").innerText = data.setup;
+  document.getElementById("delivery").innerText = data.punchline;
+}
