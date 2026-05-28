45f3cefe961984ba10664b6a3bce3be32e8da910
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-28 22:03:12 +0530

    the login function to improve code clarity

--- JavaScript/ExceptionHandling/script.js
+++ JavaScript/ExceptionHandling/script.js
@@ -1,3 +1,41 @@
+// async function login() {
+
+//   try {
+
+//     const username = prompt("Enter Username");
+
+//     // Validation
+//     if (username === "") {
+//       throw new Error("Username Cannot Be Empty");
+//     }
+
+//     // Fake Server Delay
+//     await new Promise((resolve) => {
+//       setTimeout(resolve, 2000);
+//     });
+
+//     // Username Check
+//     if (username !== "aniket") {
+//       throw new Error("Invalid Username");
+//     }
+
+//     // Success
+//     console.log("Login Successful");
+
+//   } catch (e) {
+
+//     console.log("Error :", e.message);
+
+//   } finally {
+
+//     console.log("Login Process Finished");
+
+//   }
+
+// }
+
+// login();
+
 async function login() {
 
   try {
