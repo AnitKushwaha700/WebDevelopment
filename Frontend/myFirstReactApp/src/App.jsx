24ef6de64d9f9ce5eec7961466f9b20d8533fe01
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-05-31 10:54:49 +0530

    feat: add Header and Footer components, integrate Bootstrap for styling

--- Frontend/myFirstReactApp/src/App.jsx
+++ Frontend/myFirstReactApp/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-    <div id='abcd'>My first React App</div>
-    </>
-  )
-}
+      <Header />
 
-export default App
+      <div className="main">
+        <h1 id="heading" className="bg-primary">My First React App</h1>
+
+        <div id="card">
+          <h1>Login</h1>
+
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
