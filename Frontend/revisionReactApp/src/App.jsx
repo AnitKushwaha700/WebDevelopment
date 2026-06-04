b71f091331fd73793f578f9927afbc6774835254
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-06-04 22:13:38 +0530

    feat: add counter functionality to App component for user interaction

--- Frontend/revisionReactApp/src/App.jsx
+++ Frontend/revisionReactApp/src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { FaWifi } from "react-icons/fa";
 import { IoIosSave } from "react-icons/io";
 
 import contactPage from "./assets/contactPage.png";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div className="bg-primary p-2 text-light text-center fs-5">
@@ -17,6 +20,20 @@ function App() {
           <IoIosSave className="fs-5 text-danger" />
           <span>Save Data</span>
         </button>
+      </div>
+
+      {/* Counter Section */}
+      <div className="text-center mt-4">
+        <h3>Counter: {count}</h3>
+
+        <button
+          className="btn btn-success me-2"
+          onClick={() => setCount(count + 1)}
+        >
+          Increase
+        </button>
+
+
       </div>
     </>
   );
