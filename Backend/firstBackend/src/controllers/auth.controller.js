abce15091183f5692deec361237485aa440d2d26
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-06-23 21:02:50 +0530

    feat: implement user registration and MongoDB connection

--- Backend/firstBackend/src/controllers/auth.controller.js
+++ Backend/firstBackend/src/controllers/auth.controller.js
@@ -1,11 +1,27 @@
+import User from "../models/user.model";
+
+export const RegisterUser = async (req, res) => {
+  try {
+    const { fullName, email, password, phone, gender, dob } = req.body;
+
+    if (!fullName || !email || !password || !phone || !gender || !dob) {
+      res.status(400).json({ message: "All Fields Required" });
+      return;
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(409).json({ message: "Email Already Registered" });
+    }
+
+
+  } catch (error) {}
+};
+
 export const LoginUser = (req, res) => {
   res.json({ message: " Login seccessfull from controller" });
 };
 
 export const LogoutUser = (req, res) => {
   res.json({ message: " Logout seccessfull from controller" });
-};
-
-export const RegisterUser = (req, res) => {
-  res.json({ message: " Registration seccessfull from controller" });
 };
